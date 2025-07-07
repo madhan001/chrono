ff9d14446a1d2c33ef6fd1a3f1ef77be59354c8c
xutong05 <txu25@gmu.edu>
2025-07-06 23:30:06 -0400

    change vw steer params

--- src/chrono_models/vehicle/vertiwheeler/Vertiwheeler_RotaryArm.cpp
+++ src/chrono_models/vehicle/vertiwheeler/Vertiwheeler_RotaryArm.cpp
@@ -17,7 +17,7 @@ const double Vertiwheeler_RotaryArm::m_pitmanArmRadius = 0.002;
 
 const double Vertiwheeler_RotaryArm::m_maxAngle = 45.0 * (CH_PI / 180);
 
-const ChVector3d Vertiwheeler_RotaryArm::m_pitmanArmInertiaMoments(0.02000, 0.02500, 0.00600);
+const ChVector3d Vertiwheeler_RotaryArm::m_pitmanArmInertiaMoments(0.00638, 0.00756, 0.00150);
 const ChVector3d Vertiwheeler_RotaryArm::m_pitmanArmInertiaProducts(0.0, 0.0, 0.0);
 
 // -----------------------------------------------------------------------------
@@ -33,7 +33,6 @@ const ChVector3d Vertiwheeler_RotaryArm::getLocation(PointId which) {
     switch (which) {
         // oversteers when doing a left turn
         case ARM_L:
-            // return ChVector3d(0.0718, -0.0210, 0.0086);
             return ChVector3d(0.0718, -0.0210, 0.0086);
 
         case ARM_C:
